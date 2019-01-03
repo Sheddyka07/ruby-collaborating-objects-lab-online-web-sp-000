9148629a81dd3e8af8d5ce3fbdf47915a87148c2
Kelly Sheddy <jungaka07@gmail.com>
2019-01-03 13:50:20 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,6 +5,13 @@ class MP3Importer
     @path = filepath
   end
   
-  def files()
+  def files
+     @file_array = Dir.entries(@path).select{|file| file.include?(".mp3")}
+   end
+ 
+    def import
+     self.files
+     @file_array.each{|song| Song.new_by_filename(song)}
+   end
   
 end
